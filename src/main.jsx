ffb4bbf980fabec1e404f698e9f8839a35ad89a7
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-08-28 21:23:25 +0600

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+import {BrowserRouter} from "react-router-dom"
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
