9cd9b5b2273369c8243f79126fea7d63e2412304
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-08-30 20:21:22 +0600

    auth part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
@@ -6,20 +6,34 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import SettingPage from "./pages/SettingPage/SettingPage";
 import SignUpPage from "./pages/signUpPage/signUpPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/signup" element={<SignUpPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/setting" element={<SettingPage />}></Route>
-        <Route path="/profile" element={<ProfilePage />}></Route>
-      </Routes>
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log(authUser);
+  if (isCheckingAuth && !authUser)return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader  className= "size-10 animate-spin" />
     </div>
-  );
+  )
+    return (
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/signup" element={<SignUpPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/setting" element={<SettingPage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
+        </Routes>
+      </div>
+    );
 };
 
 export default App;
---
+++ src/store/useAuthStore.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIng: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("auth/check");
+
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("errors in checkAuth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAUth: false });
+    }
+  },
+}));
