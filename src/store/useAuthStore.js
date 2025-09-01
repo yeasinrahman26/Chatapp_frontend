a04bbac59c3b597f970bb77c1c56d925558c312d
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-09-01 20:01:23 +0600

    profile page done with profile uploading

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -54,4 +54,18 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile Upload successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error)
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
