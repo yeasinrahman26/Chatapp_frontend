0ee601cdfb23db3abfea3fb3e5e4d2a5882f78a5
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-09-04 09:50:25 +0600

    theme added

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+ const{theme} =useThemeStore()
 
   useEffect(() => {
     checkAuth();
@@ -25,7 +28,7 @@ const App = () => {
       </div>
     );
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
