49900e482682bcbe9ae3707e9b600eaddab2c94c
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-09-04 10:46:12 +0600

    sidebar done

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import { useChatStore } from "../../store/useChatStore";
+import SidebarSkeleton from "./SidebarSkeleton";
+import { Users } from "lucide-react";
+
+const Sidebar = () => {
+  const { getUsers, setSelectedUser, selectedUser, users, isUserLoading } =
+    useChatStore();
+
+  const onlineUsers = [];
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  if (isUserLoading) return <SidebarSkeleton />;
+
+  return (
+    <aside
+      className="h-full w-20 lg:w-72 border-r border-base-300 flex 
+    flex-col transition-all duration-200"
+    >
+      <div className="border-b border-base-300 w-full p-5">
+        <div className="flex items-center gap-2">
+          <Users className="size-6" />
+          <span className="font-medium hidden lg:block">Contacts</span>
+        </div>
+      </div>
+      <div className="overflow-y-auto w-full px-2 py-3">
+        {users.map((user) => (
+          <button
+            key={user._id}
+            onClick={() => setSelectedUser(user)}
+            className={`w-full flex items-center gap-3 p-3 my-2 
+                rounded-2xl shadow-sm
+              transition-all duration-200 ease-in-out
+              ${
+                selectedUser?._id === user._id
+                  ? "bg-base-300 ring-2 ring-primary "
+                  : "bg-base-100 hover:bg-base-200"
+              } 
+              focus:outline-none focus:ring-2 focus:ring-primary  `}
+          >
+            {" "}
+            <div className="relative mx-auto lg:mx-0">
+              <img
+                src={user.profilePic || "/avatar.png"}
+                alt={user.name}
+                className="size-12 object-cover rounded-full"
+              />
+              {onlineUsers.includes(user._id) && (
+                <span
+                  className="absolute bottom-0 right-0 size-3 bg-green-500 
+                  rounded-full ring-2 ring-zinc-900"
+                />
+              )}
+            </div>
+            {/* User info - only visible on larger screens */}
+            <div className="hidden lg:block text-left min-w-0">
+              <div className="font-medium truncate">{user.fullName}</div>
+              <div className="text-sm text-zinc-400">
+                {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+              </div>
+            </div>
+          </button>
+        ))}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
---
+++ src/store/useChatStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+  message: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.messages);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`messages/${userId}`);
+      set({ message: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
